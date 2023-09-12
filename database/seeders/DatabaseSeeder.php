4a2f5ae7d961a8c89c9da9e8adf088899a1ee753
patrictobola <tobola92@gmail.com>
2023-09-12 19:51:04 +0200

    randomized type and technologies in DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([TypeSeeder::class, TechnologySeeder::class, ProjectSeeder::class, UserSeeder::class]);
+        $this->call([UserSeeder::class, TypeSeeder::class, TechnologySeeder::class, ProjectSeeder::class]);
     }
 }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Technology;
 use App\Models\Type;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -15,14 +16,25 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        $tech_ids = Technology::pluck('id')->toArray();
+
         for ($i = 0; $i < 20; $i++) {
 
             $project = new Project;
             $project->title = $faker->words(3, true);
+            $project->type_id = rand(1, 6);
             $project->description = $faker->text();
             $project->date = $faker->date();
             $project->thumb = $faker->imageUrl(200, 200);
             $project->save();
+
+            // Randomizzo dei dati nella relazione molti a molti
+            $project_technologies = [];
+            foreach ($tech_ids as $tech_id) {
+                if (rand(0, 1))  $project_technologies[] = $tech_id;
+            }
+
+            $project->technologies()->attach($project_technologies);
         }
     }
 }
